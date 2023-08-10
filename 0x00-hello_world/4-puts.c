f67a3eb8a0de700ab7008c6b482d557aa21aa04a
nat006 <nthatipmoloi006@g.com>
2023-08-10 08:29:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that uses puts function to print a line
+ * Return: 0
+ */n
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

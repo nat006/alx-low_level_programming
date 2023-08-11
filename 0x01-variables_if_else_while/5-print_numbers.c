3d1abc79947f934f3a1be12299a4e0195aadb992
nat006 <nthatipmoloi006@g.com>
2023-08-11 10:24:59 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+printf("%d", a);
+putchar('\n');
+return (0);
+}

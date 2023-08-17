fac5252dc67aa2038eb8c3341da20494db064af8
nat006 <nthatipmoloi006@g.com>
2023-08-17 12:24:59 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers - checks for digit (0 to 9)
+*Return: 0
+*/
+void print_numbers(void)
+{
+int c;
+for (c = 48; c < 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}

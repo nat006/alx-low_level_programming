72e876f887942088d4fb0ad4dc95e38144cf3b81
nat006 <nthatipmoloi006@g.com>
2023-08-17 15:03:09 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,14 +8,17 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3 == 0) && (i % 5 == 0))
-printf("%s", "FizzBuzz");
-else if (i % 3 == 0)
-printf("%s", "Fizz");
-else if (i % 5 == 0)
-printf("%s", "Buzz");
+if ((i % 3) == 0 && (i % 5) == 0)
+printf("FizzBuzz");
+else if ((i % 3) == 0)
+printf("Fizz");
+else if ((i % 5) == 0)
+printf("Buzz");
 else
 printf("%d", i);
+if (i == 100)
+continue;
+printf(" ");
 }
 printf("\n");
 return (0);

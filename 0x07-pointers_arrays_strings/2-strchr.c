cb52995296e9fc733c559479c8ae2cb98ef2be92
nat006 <nthatipmoloi006@g.com>
2023-08-28 08:08:49 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,8 +5,7 @@
  * @s: The string to search through
  * @c: The character to locate
  *
- * Return: Pointer to the first occurrence of the character c
- *         in the string s, or NULL if the character is not found
+ * Return: Pointer to the first occurrence of c in s, or NULL if not found.
  */
 char *_strchr(char *s, char c)
 {

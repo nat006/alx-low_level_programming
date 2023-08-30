f12f3626457d0e8bca8982e0dedcfe89c6f72ccc
nat006 <nthatipmoloi006@g.com>
2023-08-30 06:58:56 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Computes the natural square root of a number.
- * @n: The number to find the square root of.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find its square root
  *
- * Return: The natural square root of n, or -1 if n does not have a
- *         natural square root.
+ * Return: natural square root of n, -1 if n not natural square root
  */
 int _sqrt_recursion(int n)
 {
@@ -13,20 +12,21 @@ if (n < 0)
 return (-1);
 if (n == 0 || n == 1)
 return (n);
-int start = 1;
-int end = n;
-int mid;
-int sqrt;
-while (start <= end)
-{
-mid = (start + end) / 2;
-sqrt = n / mid;
-if (mid == sqrt)
-return (mid);
-if (mid < sqrt)
-start = mid + 1;
-else
-end = mid - 1;
+return (_sqrt_helper(n, 1));
 }
-return (end);
+
+/**
+ * _sqrt_helper - helper function to find the natural square root
+ * @n: the number to find its square root
+ * @i: the possible square root
+ *
+ * Return: natural square root of n, -1 if n not have natural square root
+ */
+int _sqrt_helper(int n, int i)
+{
+if (i * i == n)
+return (i);
+if (i * i > n)
+return (-1);
+return (_sqrt_helper(n, i + 1));
 }

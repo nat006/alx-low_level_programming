098c278a22d2404a0f16422ec07d16e6da16ffb5
nat006 <nthatipmoloi006@g.com>
2023-09-01 15:46:38 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the program name
+ * @argc: the number of command line arguments
+ * @argv: an array of command line argument strings
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}

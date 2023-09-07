bc3a7ca9fde1c136db18d1df4e1d078d62d38744
nat006 <nthatipmoloi006@g.com>
2023-09-07 09:18:13 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory of size b
+ * @b: size of allocation in bytes
+ *
+ * Return: pointer to allocated memory
+ * Terminate with exit status 98 on failure
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
+}

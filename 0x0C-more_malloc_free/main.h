1ca5197b229184cbb6d0c75e1239f14e09e9d110
nat006 <nthatipmoloi006@g.com>
2023-09-07 10:34:38 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,6 @@
 #define MAIN_H
 #ifndef MAIN_H
-
+int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);

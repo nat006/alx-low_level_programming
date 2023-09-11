7296058d7d96271a8b75f7d84d1c8248f5205442
nat006 <nthatipmoloi006@g.com>
2023-09-11 06:45:07 -0700

    dog.h

--- 0x0D-preprocessor/dog.h
+++ 0x0D-preprocessor/dog.h
@@ -16,3 +16,13 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+int _strlen(char *s);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+
+#endif

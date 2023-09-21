f343e54731a4ef93ff807ac02abeb575b5741182
nat006 <nthatipmoloi006@gmail.com>
2023-09-21 03:23:53 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,24 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LINKED_H
+#define LINKED_H
 
-int _putchar(char c);
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - Stringly linked list
+ * @len: length of the string
+ * @str: String.
+ * @next: Points to the next node.
+ * Description: Stringly linked list node structure.
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);

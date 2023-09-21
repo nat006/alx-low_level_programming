dc5e98482976b7be79003490d27a09cdda1390ff
nat006 <nthatipmoloi006@gmail.com>
2023-09-21 03:46:09 -0700

    lists.h

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,3 +10,8 @@ void print_before_main(void)
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
+int main(void)
+{
+print_before_main();
+return (0);
+}

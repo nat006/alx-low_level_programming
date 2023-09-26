57094bd4089a301a573232fc4f698b5164b54c26
nat006 <nthatipmoloi006@gmail.com>
2023-09-26 06:02:53 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,33 +4,44 @@
  * insert_nodeint_at_index - Inserts a new node at a given position.
  * @head: Pointer to first node of listint_t list
  * @idx: Index of list where the new node should be added. Index starts at 0
- * @n: The data that will be added to the new node
+ * @n: Integer value to be added to the new node
  *
- * Return: The address of the new node, or NULL if it failed
- * If not possible to add the new node at index idx,
- * do not add the new node and return NULL
+ * Return: Address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new_node, *temp = *head;
 	unsigned int i;
-	listint_t *new, *temp = *head;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
 
 	if (idx == 0)
-		return (add_nodeint(head, n));
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-	for (i = 0; temp != NULL && i < idx - 1; i++)
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (temp == NULL || temp->next == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
 		temp = temp->next;
+	}
 
-	if (temp == NULL)
-		return (NULL);
+	new_node->next = temp->next;
+	temp->next = new_node;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = temp->next;
-	temp->next = new;
-
-	return (new);
+	return (new_node);
 }

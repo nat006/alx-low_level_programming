da84fdb27b93c19ad1492b241d2c6775f48229eb
nat006 <nthatipmoloi006@gmail.com>
2023-09-26 07:06:32 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,51 +1,36 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * print_listint_safe - Prints a listint_t linked list.
- * @head: A pointer to the first node of the linked list.
+ * @head: A pointer to the node of the linked list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
-	size_t count = 0;
+	const listint_t *current;
+	size_t node_count = 0;
 
 	if (head == NULL)
 		exit(98);
-	tortoise = head;
-	hare = head->next;
-	while (hare != NULL && hare < tortoise)
+
+	current = head;
+
+	while (current != NULL)
 	{
-		count++;
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
-		hare = hare->next;
-		if (hare != NULL && hare < tortoise)
+		printf("[%p] %d\n", (void *)current, current->n);
+		node_count++;
+
+		if (current >= current->next)
 		{
-			count++;
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-			tortoise = tortoise->next;
-			hare = hare->next;
+			printf("-> [%p] %d\n", (void *)current->next,
+				current->next->n);
+			break;
 		}
+
+		current = current->next;
 	}
-	if (hare != NULL)
-	{
-		count++;
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
-	}
-	while (hare != NULL && hare < tortoise)
-	{
-		count++;
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
-		hare = hare->next;
-	}
-	if (hare != NULL)
-	{
-	count++;
-	printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-	}
-	return (count);
+
+	return (node_count);
 }

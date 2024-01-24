a066e45d5cc81a3c326d781c6decf34cf6141482
nat006 <nthatipmoloi006@gmail.com>
2024-01-24 07:58:41 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,22 +19,17 @@ def island_perimeter(grid):
     - Grid is completely surrounded by water, and there is one island (or nothing)
     - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island)
     """
-    if not grid:
-        return 0
-    
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
-    
-    for i in range(rows):
-        for j in range(cols):
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                perimeter += 4  # each land cell contributes 4 to the perimeter
-
-                # check the neighbors to subtract the shared sides
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2

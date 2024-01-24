960a71bf6065ab45ede8c1d3794487f7072f35fd
nat006 <nthatipmoloi006@gmail.com>
2024-01-24 08:08:33 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
 
 def island_perimeter(grid):
     """
@@ -9,15 +10,6 @@ def island_perimeter(grid):
 
     Returns:
     - the perimeter of the island
-
-    Assumptions:
-    - 0 represents a water zone
-    - 1 represents a land zone
-    - One cell is a square with side length 1
-    - Grid cells are connected horizontally/vertically (not diagonally)
-    - Grid is rectangular, width and height don’t exceed 100
-    - Grid is completely surrounded by water, and there is one island (or nothing)
-    - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island)
     """
     width = len(grid[0])
     height = len(grid)

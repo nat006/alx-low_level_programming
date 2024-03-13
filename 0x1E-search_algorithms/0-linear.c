1dad91bfdde2b031774d713d826577372836c9ac
nat006 <nthatipmoloi006@gmail.com>
2024-03-13 02:11:30 -0700

    search_algos.h

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "search_algos.h"
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+    {
+        return -1;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Trying value: %d\n", array[i]);
+        if (array[i] == value)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
